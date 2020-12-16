c6a77cc676d5be7b18c54f455a8cd76cad027e59
kyrie-leon <gaomeng9518@163.com>
2020-12-16 22:15:59 +0800

    final

--- addToArrayForm.c
+++ addToArrayForm.c
@@ -150,6 +150,90 @@ int* addToArrayForm(int* A, int ASize, int K, int* returnSize) {
 
 }
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+//逆置数组
+void reverse(int * arr, int size, int start, int end)
+{
+    while (start<end)
+    {
+        swap(&arr[start], &arr[end]);
+        start++;
+        end--;
+    }
+}
+
+
+
+//最终可以运行的版本
+/**
+* Note: The returned array must be malloced, assume caller calls free().
+*/
+int* addToArrayForm(int* A, int ASize, int K, int* returnSize) {
+    //1.计算K的位数
+    int KSize = 0;    //K的位数也是K数组的大小
+    int Knum = K;
+    while (Knum)
+    {
+        Knum /= 10;
+        KSize++;
+    }
+
+
+    int ansSize = ASize>KSize ? ASize : KSize;      //最后的结果数组长度不会超过最大数组长度+1的长度
+    int * ans = (int *)malloc(sizeof(int)*(ansSize + 1));
+    
+    //2.A和K相加的结果存入ans,
+    int nextnum=0;  //代表下一位的进位的值0或1
+    int Ai=ASize-1;   //表示Ai的每一位
+    int ansi=0;
+    //将A和K的结果相加
+    while(ansSize--)
+    {
+        int a=0;
+        //确保A数组不越界
+        if(Ai>=0)
+        {
+            a = A[Ai];
+            Ai--;
+        }
+
+
+        int ret = a + K%10+nextnum;     //每一位的结果为a数组每一位+K的每一位+进位
+        K /=10;
+        //如果结果大于10表示有进位
+        if(ret>9)
+        {
+            ret -= 10;
+            nextnum=1;
+        }
+        else
+        {
+            nextnum=0;
+        }
+        ans[ansi]=ret;
+        ansi++;
+    }
+    if (nextnum == 1)
+    {
+        ans[ansi] = 1;
+        ansi++;
+    }
+    //5.将ans逆置输出
+    reverse(ans, ansi, 0, ansi - 1);
+    *returnSize = ansi;
+    return ans;
+
+
+
+}
+
 int main()
 {
 	int K = 34;
