7322e70bf12e76255c0f3ab5f84a294c57ba8ce0
kyrie-leon <gaomeng9518@163.com>
2021-01-17 22:17:39 +0800

    104. 二叉树的最大深度

--- maxDepth.c
+++ maxDepth.c
@@ -7,10 +7,21 @@
  * };
  */
 
+//1.
+// int maxDepth(struct TreeNode* root){
+//     //只需求出根节点的左右孩子最大深度
+//     if(root == NULL)
+//         return 0;
+//     return 1+fmax(maxDepth(root->left),maxDepth(root->right));
+// }
 
+//2.第二种写法
 int maxDepth(struct TreeNode* root){
     //只需求出根节点的左右孩子最大深度
     if(root == NULL)
         return 0;
-    return 1+fmax(maxDepth(root->left),maxDepth(root->right));
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+
+    return leftDepth > rightDepth? leftDepth+1: rightDepth+1;
 }
