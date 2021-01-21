ce94c4f8276d1a27643e076627918920aebce0b9
kyrie-leon <gaomeng9518@163.com>
2021-01-21 18:06:15 +0800

    110. 平衡二叉树-2

--- isBalanced.c
+++ isBalanced.c
@@ -6,6 +6,8 @@
  *     struct TreeNode *right;
  * };
  */
+ 
+ //1.自底向上
 int maxDepth(struct TreeNode* root){
     //只需求出根节点的左右孩子最大深度
     if(root == NULL)
@@ -21,4 +23,36 @@ bool isBalanced(struct TreeNode* root){
     int leftDepth = maxDepth(root->left);
     int rightDepth = maxDepth(root->right);
     return abs(leftDepth-rightDepth)<=1 && isBalanced(root->left) && isBalanced(root->right);
+}
+
+
+//2.自顶向下
+bool _isBalanced(struct TreeNode* root, int* pdepth)
+{
+    if(root == NULL)
+    {
+        *pdepth = 0;
+        return true;
+    }
+    else
+    {
+        //左 右 根
+        int leftdepth = 0;
+        if(_isBalanced(root->left,&leftdepth)==false)
+            return false;
+        int rightdepth= 0;
+        if(_isBalanced(root->right,&rightdepth)==false)
+            return false;
+        if(abs(leftdepth-rightdepth)>1)
+            return false;
+
+        *pdepth = leftdepth>rightdepth? leftdepth+1 : rightdepth+1;
+        return true;
+    }
+}
+
+
+bool isBalanced(struct TreeNode* root){
+    int depth = 0;
+    return _isBalanced(root,&depth);
 }
