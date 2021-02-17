e38a0e08c17cad428759855757795ac9b0124f0e
kyrie-leon <gaomeng9518@163.com>
2021-02-17 21:23:13 +0800

    35. 搜索插入位置

--- searchInsert.c
+++ searchInsert.c
@@ -1,3 +1,4 @@
+
 int searchInsert(int* nums, int numsSize, int target){
     int insert = 0;
     for(int i = 0; i < numsSize; ++i)
@@ -11,6 +12,37 @@ int searchInsert(int* nums, int numsSize, int target){
             insert = i;
         }
     }
+    if(target < nums[0])
+    {
+        return 0;
+    }
+    else
+    {
+        return insert + 1;
+    }
+}
 
-        return insert+1;
+//二分法
+int searchInsert(int* nums, int numsSize, int target){
+    int left = 0;
+    int right = numsSize - 1;
+    
+    while(left <= right)
+    {
+        int mid = (left + right)/2;
+        if(target < nums[mid])
+        {
+            right = mid-1;
+        }
+        else if(target > nums[mid])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+
+    return left;
 }
